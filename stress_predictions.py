d683c722fe87539ded4037036ee12ef1ab9a1930
Aakash <aakashrj777@gmail.com>
2026-06-25 00:06:35 +0530

    Initial commit

---
+++ stress_predictions.py
@@ -0,0 +1,56 @@
+# 1. IMPORTS
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+
+
+# 2. LOAD DATA
+df = pd.read_csv("data/student_stress.csv")
+
+
+# 3. PREPROCESSING
+le = LabelEncoder()
+df['stress_level'] = le.fit_transform(df['stress_level'])
+df.fillna(df.mean(), inplace=True)
+
+X = df.drop('stress_level', axis=1)
+y = df['stress_level']
+
+
+# 4. TRAIN–TEST SPLIT
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+
+# 5. MODEL TRAINING
+model = RandomForestClassifier(
+    n_estimators=200,
+    max_depth=10,
+    random_state=42
+)
+model.fit(X_train, y_train)
+
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, cmap="Blues")
+plt.show()
+
+
+
+importance = model.feature_importances_
+for f, i in zip(X.columns, importance):
+    print(f, ":", i)
